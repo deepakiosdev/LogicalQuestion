1e69f96ed0d97731d29111809d7127dd4ace3eb4
Deepak <deepak.262903@gmail.com>
2015-12-20 13:03:16 +0530

    Initial Commit

---
+++ LogicalQuestion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LogicalQuestion
+//
+//  Created by Deepak on 20/12/15.
+//  Copyright © 2015 Deepak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LogicalQuestion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LogicalQuestion
+//
+//  Created by Deepak on 20/12/15.
+//  Copyright © 2015 Deepak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
